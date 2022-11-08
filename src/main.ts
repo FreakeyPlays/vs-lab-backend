20e69bfc3739c08f14d933f970c8cde86df9b64e
FreakeyPlays <chrismerck@gmx.de>
2022-11-08 11:25:14 +0100

    feat(api): added swagger Documentation page

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('ToDo')
+    .setDescription('The ToDo-Api from the VS Lab.')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(process.env.SERVER_PORT || 3000);
 }
 bootstrap();
