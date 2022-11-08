5914d56ce5692ad7860ea1720dcbee701570f970
FreakeyPlays <chrismerck@gmx.de>
2022-11-08 11:26:13 +0100

    feat(api): endabled CORS

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
 
+  app.enableCors();
   await app.listen(process.env.SERVER_PORT || 3000);
 }
 bootstrap();
