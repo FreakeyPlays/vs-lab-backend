6c1f9991c3faa7fb74dcfbfac58549297ea30701
FreakeyPlays <chrismerck@gmx.de>
2022-11-08 11:25:46 +0100

    feat(api): added global Prefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   const config = new DocumentBuilder()
     .setTitle('ToDo')
     .setDescription('The ToDo-Api from the VS Lab.')
